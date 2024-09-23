feb9e9dd5743305cdf5c4c3af9bcb2c87f9142b4
German <noiconuf@gmail.com>
2024-09-23 18:04:53 -0300

    Se saco la authorizacion para cargar los productos es decir no tiene sentido obligar a alguien a que se loggee para mirar los productos

--- docs/info.js
+++ docs/info.js
@@ -8,7 +8,7 @@ export const info = {
         },
         servers: [  
             {
-                url: 'http://localhost:8080/'
+                url: 'http://localhost:3000/'
             },
             {
 
